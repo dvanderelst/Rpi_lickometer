4d300ba73d36d7627e421e6a777db65f2e74801d
Lenovo <dieter.vanderelst@fastmail.net>
2022-11-15 09:05:52 -0500

    Added test for screen

---
+++ run_test_screen.py
@@ -0,0 +1,9 @@
+from LickLibrary import myOLED
+
+screen = myOLED.myOLED()
+
+screen.set_text(0, 'test line 0')
+screen.set_text(1, 'test line 1')
+screen.set_text(2, 'test line 2')
+screen.set_text(3, 'test line 3')
+
